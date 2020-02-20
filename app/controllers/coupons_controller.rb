4edd38ab2db3612c777dea4a4f48f1130227ffdf
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-20 09:58:39 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,8 +5,11 @@ class CouponsController < ApplicationController
   end
 
   def new
-
   end
+
+  def show 
+    @coupon = Coupons.find_by(params[:id])
+  end 
 
   def create
     @coupon = Coupons.create(coupon_code: params[:coupon_code], store: params[:store])
