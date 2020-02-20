470a3ab59a9e4e034f9a0bc4ab87ef11fec239ff
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-20 10:07:09 +0000

    Automatically backed up by Learn

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -12,7 +12,7 @@ class CouponController < ApplicationController
   end
 
   def create
-    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    @coupon = Coupon.create(coupon: params[:coupon_code], store: params[:store])
     redirect_to coupons_path(@coupon)
   end
 
